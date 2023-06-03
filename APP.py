6a2f1f09c482cf9c1291dd1b3bb16fae7509195e
NITHEESH KUMAR REDDY LEBAKA <76637901+nith2349@users.noreply.github.com>
2023-06-03 18:42:43 +0530

    First Commit

---
+++ APP.py
@@ -0,0 +1,57 @@
+from tkinter import *
+import requests
+import time
+
+def openweatherdata():
+    citiesdata = dataentry.get()
+    # apikey = 3236dede3db9ce72ee32bee8b8ae69ca
+    apilink = "https://api.openweathermap.org/data/2.5/weather?q=" + citiesdata + "&appid=3236dede3db9ce72ee32bee8b8ae69ca"
+    #K to C
+    ktoc = 273.2
+    json = requests.get(apilink).json()
+    apidata = json['weather'][0]['main']
+    temparature = int(json['main']['temp']-ktoc)
+    feelslike = int(json['main']['feels_like']-ktoc)
+    lowest = int(json['main']['temp_min']-ktoc)
+    highest = int(json['main']['temp_max']-ktoc)
+    pressure = json['main']['pressure']
+    humidity = json['main']['humidity']
+    visibility = json['visibility']
+    windspeed = json['wind']['speed']
+
+    maindata = apidata + "\n" + str(temparature) + "C"
+    additionalinfo = "\n" + "Feels like :" + str(feelslike) + "C" + "\n" + "Lowest :" + str(lowest) + "C" + "\n" + "Highest :" + str(highest) + "C" +"\n" + "Pressure :" + str(pressure) + "\n" + "Humidity :" + str(humidity) + "\n" + "Wind Speed :" + str(windspeed) + "\n" + "Visibility :" + str(visibility) 
+
+    #display data
+    data1.config(text=maindata)
+    data2.config(text=additionalinfo)
+
+
+#init Tkinter
+weatherapp = Tk()
+weatherapp.title("Weather App")
+weatherapp.geometry("720x1280")
+weatherapp.configure(bg = "white")
+
+#Font
+font1 = ("times",20,"bold")
+font2 = ("times",45,"bold")
+
+#Label
+data1 = Label(weatherapp, font = font2)
+data2 = Label(weatherapp, font = font1)
+
+#entry box
+dataentry = Entry(weatherapp, justify='center', width=30,font=data2)
+dataentry.focus()
+dataentry.bind('<Return>', openweatherdata)
+
+checkbutton =  Button(weatherapp, text ="check",command = openweatherdata)
+
+# Building Interface
+dataentry.pack(pady=35)
+data1.pack()
+data2.pack()
+checkbutton.pack(pady=45)
+
+weatherapp.mainloop()
